f9d654f6abffd7d6a36d1af885123156b8eb7a42
pedram <peeeedii@gmail.com>
2025-08-08 22:54:45 +0330

    doc: add some comments for better understanding the tx transaction test case

--- types/transaction_test.go
+++ types/transaction_test.go
@@ -10,22 +10,22 @@ import (
 )
 
 func TestTransaction(t *testing.T) {
-	fromPrivKey := crypto.NewPrivateKey()
-	toPrivKey := crypto.NewPrivateKey()
+	senderPrivKey := crypto.NewPrivateKey()
+	receiverPrivKey := crypto.NewPrivateKey()
 
 	input := &proto.TxInput{
-		PrevTxHash:   utils.RandomHash(),
-		PrevOutIndex: 0,
-		PublicKey:    fromPrivKey.Public().Bytes(),
+		PrevTxHash:   utils.RandomHash(), // points to a previous transaction that created a UTXO
+		PrevOutIndex: 0, // selects which output of that transaction (e.g. output 0)
+		PublicKey:    senderPrivKey.Public().Bytes(), // Who owns the coin
 	}
 
 	output1 := &proto.TxOutput{
 		Amount:  10,
-		Address: toPrivKey.Public().Address().Bytes(),
+		Address: receiverPrivKey.Public().Address().Bytes(),
 	}
 	output2 := &proto.TxOutput{
 		Amount:  90,
-		Address: fromPrivKey.Public().Address().Bytes(),
+		Address: senderPrivKey.Public().Address().Bytes(),
 	}
 
 	tx := &proto.Transaction{
@@ -34,7 +34,7 @@ func TestTransaction(t *testing.T) {
 		Outputs: []*proto.TxOutput{output1, output2},
 	}
 
-	sign := SignTransaction(tx, fromPrivKey)
+	sign := SignTransaction(tx, senderPrivKey)
 
 	input.Signature = sign.Bytes()
 
