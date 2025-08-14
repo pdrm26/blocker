6e5bbe08be9b8f6b5d5d186968ba02c008f1373d
pedram <peeeedii@gmail.com>
2025-08-14 23:27:39 +0330

    feat: add the height of the chain

--- node/chain.go
+++ node/chain.go
@@ -6,18 +6,46 @@ import (
 	"github.com/pdrm26/blocker/proto"
 )
 
+type HeaderList struct {
+	headers []*proto.Header
+}
+
+func NewHeaderList() *HeaderList {
+	return &HeaderList{
+		headers: []*proto.Header{},
+	}
+}
+
+func (h *HeaderList) Add(header *proto.Header) {
+	h.headers = append(h.headers, header)
+}
+
+func (h *HeaderList) Len() int {
+	return len(h.headers)
+}
+
+func (h *HeaderList) Height() int {
+	return h.Len() - 1
+}
 
 type Chain struct {
 	blockStore BlockStorer
+	headers    *HeaderList
 }
 
 func NewChain(bs BlockStorer) *Chain {
 	return &Chain{
 		blockStore: bs,
+		headers:    NewHeaderList(),
 	}
 }
 
+func (c *Chain) Height() int {
+	return c.headers.Height()
+}
+
 func (c *Chain) AddBlock(block *proto.Block) error {
+	c.headers.Add(block.Header)
 	return c.blockStore.Put(block)
 }
 
